851cfff704d39db1d4e3c0eb40b8cc9b65681a7f
Lee <Lee@Lees-Laptop.home>
2016-09-08 19:36:38 -0400

    I added My github account name

--- src/pkgMain/cMain.java
+++ src/pkgMain/cMain.java
@@ -3,7 +3,9 @@ package pkgMain;
 public class cMain {
 
 	public static void main(String[] args) {
-
+			/*
+			 * My Github account name is Lee-Ennis
+			 */
 		Rectangle rec = new Rectangle(1.5, 2.5);		
 		System.out.println("Rectangle Area: " + rec.Area());
 	}
